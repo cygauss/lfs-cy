6b699b1379966f234c70f95ce6e3d19db63f7bf9
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-03 01:29:34 +0800

    更新 install.sh

--- sources/install.sh
+++ sources/install.sh
@@ -277,7 +277,7 @@ touch /var/log/{btmp,lastlog,faillog,wtmp}
 chgrp -v utmp /var/log/lastlog
 chmod -v 664  /var/log/lastlog
 chmod -v 600  /var/log/btmp
-# env is above
+
 bash /sources/notpm gettext
 bash /sources/notpm bison
 bash /sources/notpm perl
