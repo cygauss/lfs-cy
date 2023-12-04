1f86c7ad84cbaf2434d4278d1b77e52acb1f411f
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-04 13:34:27 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,114 +1,21 @@
-read -p "1.check host 2.pass: " v
+export MAKEFLAGS='-j16'
+#ensure the partition mkfsed
+export LFS_PART=/dev/nvme0n1p3
+#the dir of sources(don't put / at end)
+export LFS_SOURCES=/root/sources
 
-if [ $v -eq 1 ];then
-cat > version-check.sh << "EOF"
-#!/bin/bash
-# A script to list version numbers of critical development tools
-
-# If you have tools installed in other directories, adjust PATH here AND
-# in ~lfs/.bashrc (section 4.4) as well.
-
-LC_ALL=C 
-PATH=/usr/bin:/bin
-
-bail() { echo "FATAL: $1"; exit 1; }
-grep --version > /dev/null 2> /dev/null || bail "grep does not work"
-sed '' /dev/null || bail "sed does not work"
-sort   /dev/null || bail "sort does not work"
-
-ver_check()
-{
-   if ! type -p $2 &>/dev/null
-   then
-     echo "ERROR: Cannot find $2 ($1)"; return 1; 
-   fi
-   v=$($2 --version 2>&1 | grep -E -o '[0-9]+\.[0-9\.]+[a-z]*' | head -n1)
-   if printf '%s\n' $3 $v | sort --version-sort --check &>/dev/null
-   then
-     printf "OK:    %-9s %-6s >= $3\n" "$1" "$v"; return 0;
-   else
-     printf "ERROR: %-9s is TOO OLD ($3 or later required)\n" "$1"; 
-     return 1; 
-   fi
-}
-
-ver_kernel()
-{
-   kver=$(uname -r | grep -E -o '^[0-9\.]+')
-   if printf '%s\n' $1 $kver | sort --version-sort --check &>/dev/null
-   then 
-     printf "OK:    Linux Kernel $kver >= $1\n"; return 0;
-   else 
-     printf "ERROR: Linux Kernel ($kver) is TOO OLD ($1 or later required)\n" "$kver"; 
-     return 1; 
-   fi
-}
-
-# Coreutils first because-sort needs Coreutils >= 7.0
-ver_check Coreutils      sort     7.0 || bail "--version-sort unsupported"
-ver_check Bash           bash     3.2
-ver_check Binutils       ld       2.13.1
-ver_check Bison          bison    2.7
-ver_check Diffutils      diff     2.8.1
-ver_check Findutils      find     4.2.31
-ver_check Gawk           gawk     4.0.1
-ver_check GCC            gcc      5.1
-ver_check "GCC (C++)"    g++      5.1
-ver_check Grep           grep     2.5.1a
-ver_check Gzip           gzip     1.3.12
-ver_check M4             m4       1.4.10
-ver_check Make           make     4.0
-ver_check Patch          patch    2.5.4
-ver_check Perl           perl     5.8.8
-ver_check Python         python3  3.4
-ver_check Sed            sed      4.1.5
-ver_check Tar            tar      1.22
-ver_check Texinfo        texi2any 5.0
-ver_check Xz             xz       5.0.0
-ver_kernel 4.14
-
-if mount | grep -q 'devpts on /dev/pts' && [ -e /dev/ptmx ]
-then echo "OK:    Linux Kernel supports UNIX 98 PTY";
-else echo "ERROR: Linux Kernel does NOT support UNIX 98 PTY"; fi
-
-alias_check() {
-   if $1 --version 2>&1 | grep -qi $2
-   then printf "OK:    %-4s is $2\n" "$1";
-   else printf "ERROR: %-4s is NOT $2\n" "$1"; fi
-}
-echo "Aliases:"
-alias_check awk GNU
-alias_check yacc Bison
-alias_check sh Bash
-
-echo "Compiler check:"
-if printf "int main(){}" | g++ -x c++ -
-then echo "OK:    g++ works";
-else echo "ERROR: g++ does NOT work"; fi
-rm -f a.out
-EOF
-bash version-check.sh
-rm version-check.sh
-fi
-
-read -p "LFS_FLAGS=" j
-read -p "the disk part for lfs: /dev/" d
-read -p "the dir of sources(don't put / at end): " s
-read -p "1.continue 2.exit: " i
-if [ $i -eq 2 ];then
-exit
-fi
 export LFS=/mnt/lfs
-umount /mnt/lfs
-umount /mnt
-mkfs.ext4 /dev/$d
-mount /dev/$d $LFS
+mkdir -pv $LFS
+
+mount -v $LFS_PART $LFS
+
 mkdir -v $LFS/sources
-cp $s/* $LFS/sources
-echo $j > $LFS/sources/env
 chmod -v a+wt $LFS/sources
+cp $LFS_SOURCES/* $LFS/sources
+echo $MAKEFLAGS > $LFS/sources/makeflags
 chown root:root $LFS/sources/*
-mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+
+mkdir -p $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
 
 for i in bin lib sbin; do
   ln -sv usr/$i $LFS/$i
@@ -117,15 +24,16 @@ done
 case $(uname -m) in
   x86_64) mkdir -pv $LFS/lib64 ;;
 esac
+
 mkdir -pv $LFS/tools
 groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+
 chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
 case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
 
-#only part changed, seleted in OVER ,!!!edit 2 makeflags!!!
 [ ! -e /etc/bash.bashrc ] || mv -v /etc/bash.bashrc /etc/bash.bashrc.NOUSE
 
 su - lfs  << "END"
@@ -144,7 +52,7 @@ PATH=/usr/bin
 if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
 PATH=$LFS/tools/bin:$PATH
 CONFIG_SITE=$LFS/usr/share/config.site
-MAKEFLAGS=$(cat /mnt/lfs/sources/env)
+MAKEFLAGS=$(cat /mnt/lfs/sources/makeflags)
 NOT_SOURCES=/mnt/lfs/sources
 export LFS LC_ALL LFS_TGT PATH CONFIG_SITE MAKEFLAGS NOT_SOURCES
 EOF
@@ -153,11 +61,11 @@ EOF
 
 . ~/.bashrc
 
-NOT_DIF=".cc"  bash /mnt/lfs/sources/notpm binutils
-NOT_DIF=".cc"  bash /mnt/lfs/sources/notpm gcc
-NOT_DIF=".cc-headers" bash /mnt/lfs/sources/notpm linux
-NOT_DIF=".cc"  bash /mnt/lfs/sources/notpm glibc
-NOT_DIF=".cc-libstdc++"  bash /mnt/lfs/sources/notpm gcc
+NOT_DIF=".cc"  bash $LFS/sources/notpm binutils
+NOT_DIF=".cc"  bash $LFS/sources/notpm gcc
+NOT_DIF=".cc-headers" bash $LFS/sources/notpm linux
+NOT_DIF=".cc"  bash $LFS/sources/notpm glibc
+NOT_DIF=".cc-libstdc++"  bash $LFS/sources/notpm gcc
 export NOT_DIF=".tt"
 bash /mnt/lfs/sources/notpm m4
 bash /mnt/lfs/sources/notpm ncurses
@@ -199,12 +107,10 @@ chroot "$LFS" /usr/bin/env -i   \
     TERM="$TERM"                \
     PS1='(lfs chroot) \u:\w\$ ' \
     PATH=/usr/bin:/usr/sbin     \
-    MAKEFLAGES=$(cat /mnt/lfs/sources/env)   \
+    MAKEFLAGS="$MAKEFLAGS"      \
     NOT_DIF=.tt                 \
-    NOT_SOURCES=sources         \
+    NOT_SOURCES=/sources        \
    /bin/bash --login << "END"
-#OVER
-
 mkdir -pv /{boot,home,mnt,opt,srv}
 mkdir -pv /etc/{opt,sysconfig}
 mkdir -pv /lib/firmware
@@ -272,7 +178,7 @@ touch /var/log/{btmp,lastlog,faillog,wtmp}
 chgrp -v utmp /var/log/lastlog
 chmod -v 664  /var/log/lastlog
 chmod -v 600  /var/log/btmp
-# env is above
+
 bash /sources/notpm gettext
 bash /sources/notpm bison
 bash /sources/notpm perl
@@ -282,11 +188,13 @@ bash /sources/notpm util-linux
 rm -rf /usr/share/{info,man,doc}/*
 find /usr/{lib,libexec} -name \*.la -delete
 rm -rf /tools
-
 END
 
 mountpoint -q $LFS/dev/shm && umount $LFS/dev/shm
 umount $LFS/dev/pts
 umount $LFS/{sys,proc,run,dev}
 cd $LFS
-tar -cJpf $HOME/lfs-temp-tools-12.0.tar.xz .
+tar -cpf ~/lfs-temp-tools-12.0.tar .
+
+userdel -r lfs
+[ ! -e /etc/bash.bashrc.NOUSE ] || mv -v /etc/bash.bashrc.NOUSE /etc/bash.bashrc
