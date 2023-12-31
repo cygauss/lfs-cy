282800864e5c001856d75e3c2c29dcc2cd350f2f
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-31 22:33:37 +0800

    Update gcc-3.sh

--- sources/gcc-3.sh
+++ sources/gcc-3.sh
@@ -1,9 +1,9 @@
-tar -xf ../mpfr-4.2.0.tar.xz
-mv -v mpfr-4.2.0 mpfr
-tar -xf ../gmp-6.3.0.tar.xz
-mv -v gmp-6.3.0 gmp
-tar -xf ../mpc-1.3.1.tar.gz
-mv -v mpc-1.3.1 mpc
+tar -xf ../mpfr*tar*
+mv -v mpfr-* mpfr
+tar -xf ../gmp*tar*
+mv -v gmp-* gmp
+tar -xf ../mpc*tar*
+mv -v mpc-* mpc
 case $(uname -m) in
   x86_64)
     sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
