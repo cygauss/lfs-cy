599357cb7923a53419a66df0347b5bb765d265d6
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-31 23:02:23 +0800

    Update grub-1.sh

--- sources/grub-1.sh
+++ sources/grub-1.sh
@@ -6,3 +6,15 @@
             --disable-werror'
             make
             make install
+
+mkdir -pv $LFS/boot/grub
+cat > $LFS/boot/grub/grub.cfg << "EOF"
+# Begin /boot/grub/grub.cfg
+set default=0
+set timeout=5
+
+menuentry "LFS Temporary System" {
+        linux   /vmlinuz root=/dev/sda2 rw init=/bin/bash
+        boot
+}
+EOF
