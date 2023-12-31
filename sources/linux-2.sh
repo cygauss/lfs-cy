53454abbe4d62133de926a1d415129cfb73d9120
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-31 23:57:18 +0800

    Update linux-2.sh

--- sources/linux-2.sh
+++ sources/linux-2.sh
@@ -1,4 +1,5 @@
 make mrproper
-make ARCH=x86 CROSS_COMPILE=$LFS_TGT- menuconfig
+cp ../linux.config .config
+make ARCH=x86 CROSS_COMPILE=$LFS_TGT- oldconfig
 make ARCH=x86 CROSS_COMPILE=$LFS_TGT-
 install -vm644 arch/x86/boot/bzImage $LFS/boot/vmlinuz
