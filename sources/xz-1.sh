f8c6560efd4b94b623b13e8b1778cb196aae2336
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-31 23:14:30 +0800

    Update xz-1.sh

--- sources/xz-1.sh
+++ sources/xz-1.sh
@@ -2,7 +2,7 @@
             --host=$LFS_TGT                   \
             --build=$(build-aux/config.guess) \
             --disable-static                  \
-            --docdir=/usr/share/doc/xz-$VERXZ
+            --docdir=/usr/share/doc/xz-5.4.5
             make
 make DESTDIR=$LFS install
 rm -v $LFS/usr/lib/liblzma.la
