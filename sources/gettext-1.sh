fc0909fdaa947a22741a65d8715118ae3e0ab152
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-31 22:47:50 +0800

    Update gettext-1.sh

--- sources/gettext-1.sh
+++ sources/gettext-1.sh
@@ -1,4 +1,6 @@
-./configure --disable-shared
+./configure --disable-shared /
+            --host=$LFS_TGT                   \
+            --build=$(build-aux/config.guess)
 --disable-shared
 make
-cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /usr/bin
+cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} $LFS/usr/bin
