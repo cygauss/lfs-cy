ace0c54ceda2b31a6ebf7a3914c997cb74541663
cygauss <117000961+cygauss@users.noreply.github.com>
2023-12-31 22:48:25 +0800

    Update bison-1.sh

--- sources/bison-1.sh
+++ sources/bison-1.sh
@@ -1,5 +1,7 @@
 ./configure --prefix=/usr \
-            --docdir=/usr/share/doc/bison-3.8.2
+            --docdir=/usr/share/doc/bison-3.8.2 \
+                        --host=$LFS_TGT                   \
+            --build=$(build-aux/config.guess) \
 --docdir=/usr/share/doc/bison-3.8.2
 make
 make install
